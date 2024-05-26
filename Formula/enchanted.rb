e5f9aaf535626d2f808a7aafc90ac79468b9344e
Augustinas Malinauskas <augustinasmal@gmail.com>
2024-05-26 18:44:34 +0100

    fix

--- Formula/enchanted.rb
+++ Formula/enchanted.rb
@@ -2,7 +2,7 @@ class Enchanted < Formula
   desc "Enchanted is open source, Ollama compatible, elegant macOS/iOS/iPad app for working with privately hosted models such as Llama 2, Mistral, Vicuna, Starling and more. It's essentially ChatGPT app UI that connects to your private models. The goal of Enchanted is to deliver a product allowing unfiltered, secure, private and multimodal experience across all of your devices in iOS ecosystem."
   homepage "https://github.com/AugustDev/enchanted"
   url "https://github.com/AugustDev/enchanted/releases/download/v1.6.7/Enchanted.tar.gz"
-  sha256 "899dd4890279d1ef8e8f9d6141f8a166ade8686aebdbc165d44a5eea51dd2701"
+  sha256 "65a8739e42353db725d29b2f4b54f82a7a6f23eb3c6d433d14d651e637fbf573"
 
   def install
     mkdir_p prefix/"Enchanted.app"
